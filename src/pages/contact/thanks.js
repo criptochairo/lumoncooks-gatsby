deb60684e22528cfa876abce5f1b9eecc56b294b
Criptochairo <luis.cardenases@gmail.com>
2022-05-11 17:33:28 -0500

    changed lang in thanks form and changed og image for whatsapp icons

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,8 +7,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>¡Gracias por ponerte en contacto!</h1>
+          <p>Te responderemos a la brevedad.</p>
         </div>
       </div>
     </section>
